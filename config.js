8570fc36c04927eeaf649319c439fe5727259b81
long1601 <106688588+long1601@users.noreply.github.com>
2022-06-02 00:02:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    titleWeb: "vui vẻ ngày mưa",
+    titleWeb: "1 chút ngáo ngơ",
     introTitle: 'Lương Ngọc Anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
