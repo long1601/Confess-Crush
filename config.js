8fdcb6dd5fa87467a716963b3afc96a57a93f542
long1601 <106688588+long1601@users.noreply.github.com>
2022-06-01 22:54:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
+    titleWeb: "vui vẻ ngày mưa",
     introTitle: 'Lương Ngọc Anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^A Nhong^^',
-    title: 'Phải chăng em ta chẳng được đi chơi với nhau 🥰',
+    title: 'Phải chăng anh chẳng được đi chơi với em 🥰',
     desc: 'Mình đi chơi được không ạ ',
     btnYes: 'hokk <33',
     btnNo: 'đi thôi :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao chẳng thể hẹn em <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Vì em thích anh <33333333',
+    reply: 'Vì em thích anh nên em ngại <33333333',
     mess: 'Anh biết mà 🥰. Anh đợi em 😘😘',
     messDesc: 'Hôm nào rảnh em đưa anh đi nha.',
     btnAccept: 'Okiiiii lun <3',
